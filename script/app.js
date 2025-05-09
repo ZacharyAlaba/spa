f4f7d366ccbade979a1eb791ecd29af30e77e77b
ZacharyAlaba <zachary.alaba@urios.edu.ph>
2025-05-09 09:22:22 +0000

    Second Oms

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3383/cheese/api/",  
+    apiBaseUrl:"http://172.17.100.14:3383/alaba6/api/",  
     appName: "Electronic store",  
 });
